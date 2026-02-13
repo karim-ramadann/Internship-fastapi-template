7537cdb148ba602e82bd7f39bf320297c840c774
Rashad <r.saab94@gmail.com>
2026-02-13 17:04:44 +0200

    add account id management

--- infrastructure/terraform/runtime/locals.tf
+++ infrastructure/terraform/runtime/locals.tf
@@ -1,9 +1,23 @@
 locals {
+  # AWS Account IDs per Environment
+  # TODO: Update these with your actual AWS account IDs (12-digit numbers)
+  # Set to null to disable validation for a specific environment
+  account_ids = {
+    dev        = null # "123456789012"  # Development account
+    staging    = null # "234567890123"  # Staging account
+    production = null # "345678901234"  # Production account
+  }
+
+  # Current AWS account information
+  account_id          = data.aws_caller_identity.current.account_id
+  expected_account_id = lookup(local.account_ids, var.environment, null)
+
   # Context object passed to all modules
   context = {
     project     = var.project
     environment = var.environment
     region      = var.aws_region
+    account_id  = local.account_id
     common_tags = merge(
       var.common_tags,
       {
